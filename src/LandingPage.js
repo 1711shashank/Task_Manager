71d96095e9d1ca590d524e33cc90b6a9d87ad43c
1711shashank <1711shashank@gmail.com>
2023-03-10 15:07:22 +0530

    landing page and google button

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,10 +1,21 @@
+import { Button } from '@mui/material'
 import React from 'react'
+import './LandingPage.css'
+
 
 const LandingPage = () => {
     return (
         <>
-            <div className='body'>
-                <h1> This is landing Page </h1>
+            <div className='LandingPage'>
+                <div className='LandingPage-left'>
+                </div>
+                <div className='LandingPage-right'>
+
+                    <Button variant="outlined" className='LandingPage-rightButton'>
+                        <img src={"https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"} alt="my" width={"20px"} />
+                        <p>Sign in with Google</p> 
+                    </Button>
+                </div>
             </div>
         </>
     )
